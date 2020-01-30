2e21fe46f65d5d8114875eb33824546682893471
Paola Dolcemascolo <dolcemascop1@gmail.com>
2020-01-30 18:21:22 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -3,7 +3,7 @@ def create_an_empty_array
 end
 
 def create_an_array
-  array = ["London", "Banjo", "Ciccio", "Gracie"]
+  dogs = ["London", "Banjo", "Ciccio", "Gracie"]
 end
 
 def add_element_to_end_of_array(array, element)
